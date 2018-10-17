c672cf67d5dd1023906f2a82e52703c4e36cc916
Israel Oluwole <olaleyeisrael@gmail.com>
2018-10-17 23:00:02 +0100

    first commmit UIupdate

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <title>Weather App</title>
     <link rel="manifest" href="weather.webmanifest">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+<header>
+    <div class="navbar">
+        <div class="appTitle boxAlign">
+            <p>WeatherBy</p>
+        </div>
+        <div class="searchBar boxAlign">
+            <button></button>   
+        </div>
+    </div>
+</header>
+
+<!-- 
 <header>
     <h1>The Super Awesome Weather App</h1>
 </header>
@@ -22,7 +34,20 @@
         <div class="weatherDescription">Weather Description</div>
         <div class="weatherTemperature">Temperature</div>
     </div>
-</div>
+</div> -->
+<script src="https://www.gstatic.com/firebasejs/5.5.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCGImpRiHMInrz0znVhKt5IwX3CVFHS_oU",
+    authDomain: "weatherby-9ffac.firebaseapp.com",
+    databaseURL: "https://weatherby-9ffac.firebaseio.com",
+    projectId: "weatherby-9ffac",
+    storageBucket: "weatherby-9ffac.appspot.com",
+    messagingSenderId: "480566564233"
+  };
+  firebase.initializeApp(config);
+</script>
 <script src="app.js"></script>
 </body>
 </html>
